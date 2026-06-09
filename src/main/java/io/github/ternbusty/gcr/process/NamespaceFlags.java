63569d707b0025f548dbf2d03a6dc7092642d0c7
ternbusty <sacrament.dev@gmail.com>
2026-06-09 09:14:33 +0900

    feat: add rootfs pivot, cgroup v2 setup, and namespace flag mapping

---
+++ src/main/java/io/github/ternbusty/gcr/process/NamespaceFlags.java
@@ -0,0 +1,28 @@
+package io.github.ternbusty.gcr.process;
+
+import io.github.ternbusty.gcr.spec.Spec;
+import io.github.ternbusty.gcr.syscall.Constants;
+
+import java.util.List;
+
+public final class NamespaceFlags {
+    private NamespaceFlags() {}
+
+    public static int fromSpec(List<Spec.Namespace> namespaces) {
+        if (namespaces == null) return 0;
+        int flags = 0;
+        for (Spec.Namespace ns : namespaces) {
+            flags |= switch (ns.type) {
+                case "mount" -> Constants.CLONE_NEWNS;
+                case "network" -> Constants.CLONE_NEWNET;
+                case "uts" -> Constants.CLONE_NEWUTS;
+                case "ipc" -> Constants.CLONE_NEWIPC;
+                case "pid" -> Constants.CLONE_NEWPID;
+                case "user" -> Constants.CLONE_NEWUSER;
+                case "cgroup" -> Constants.CLONE_NEWCGROUP;
+                default -> 0;
+            };
+        }
+        return flags;
+    }
+}
